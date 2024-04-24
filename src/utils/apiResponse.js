c5bff26456573fcafa9b73160a4aa887b5225dae
JayeshPC <Jayeshsutar1108@gmail.com>
2024-04-24 17:54:02 +0530

    custom API response utilities are created

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,13 @@
+class ApiSuccess {
+    constructor(
+        statusCode,
+        data,
+        message = "Success"
+    )
+    {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//  Promise way Method handler
+ const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>{
+        Promise
+        .resolve((requestHandler(req,res,next)))
+        .catch((err)=>{ next(err)})
+    }
+ }
+
+ export {asyncHandler}
+
+// Try Catch method 
+// const asyncHandler = (fn) = async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
